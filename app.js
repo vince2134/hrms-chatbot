17f3af2c1ef895fcb4d374c3e84776ba8648a304
SIVA SANKAR <mail@sivasankar.in>
2016-06-20 08:08:55 +0530

    Added Router
    Index Router / Defined

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ var token = "EAAZAEEXXtK1YBANWR62ZCbVi31fkcQaeSEqGjW8VNdGwhjRB8UDxleUmDH91MBsFZA
 
 var app = express();
 app.set('port', (process.env.PORT || 1000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
 })
 
 
-app.get('/webhook', function(req, res) {
+app.get('/', function (req, res) {
+  res.send('hello world i am a secret bot')
+});
+
+app.get('/webhook', function (req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === token) {
+    req.query['hub.verify_token'] === token) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+    res.sendStatus(403);
+  }
 });
