1976247c73a4aabb1046059c128921834b1730f4
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-01-23 10:05:14 +0800

    update

--- app.js
+++ app.js
@@ -130,12 +130,9 @@ function receivedMessage(event) {
 
   request.on('response', function(response) {
     console.log("INTENT NAME: " + response.result.metadata.intentName);
-    count = Object.keys(response.result.parameters.date).length;
-    console.log(count);
-    var count = Object.keys(response.result.parameters.end_date).length;
-    console.log(count);
-    count = Object.keys(response.result.parameters.LeaveType).length;
-    console.log(count);
+    console.log(response.result.parameters.date);
+    console.log(response.result.parameters.end_date);
+    console.log(response.result.parameters.LeaveType);
     console.log(response);
   });
 
