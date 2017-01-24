4029b5f0ce4bcb899a739fd396a0950e5f94e581
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-01-24 11:41:31 +0800

    update

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ var token = "EAAFJiEO72j4BAD6HkTpQSbzzYLYmGRMey68u40DKmOrj5pDfsX54AJtpBM7oDn6ZAA
         return;
       }
       console.log('Connection established');
+      console.log(notified);
       var myVar = setInterval(function(){ myTimer() }, 1000);
 
       function myTimer() {
