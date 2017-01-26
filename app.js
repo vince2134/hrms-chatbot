e5db51cad59062ba241ab05cde75f75017cf5949
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-01-26 10:46:27 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -74,22 +74,7 @@ request.on('response', function(response) {
     console.log("REQUEST");
     setIntent(response);
     if(intent == "file_leave")
-        //callQuery(response);
-        {
-             con.query("SELECT * FROM users where USERNAME = '" + "atan" + "';", function(err, rows) {
-        if (err) throw err;
-
-        console.log('CHECK REGISTER Data received from Db:\n');
-        console.log(rows);
-        console.log("LENGTH: " + rows.length);
-
-        if (rows.length > 0) {
-            console.log("Rows returned: " + rows.length);
-            register = true;
-        }
-        console.log("END OF QUERY");  
-    });
-        }
+    {callQuery(response);}
 });
 
 request.on('error', function(error) {
