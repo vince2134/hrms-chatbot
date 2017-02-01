d026d802b58760df0502b812fc8603e76080a695
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-01 17:37:38 +0800

    Add files via upload

--- basetrigger.js
+++ basetrigger.js
@@ -16,36 +16,7 @@ var body = JSON.stringify({
     foo: "bar"
 })
 
-http.get('http://13.76.85.160:443/notifyusers', (res) => {
-  const statusCode = res.statusCode;
-  const contentType = res.headers['content-type'];
-
-  let error;
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\n` +
-                      `Status Code: ${statusCode}`);
-  } else if (!/^application\/json/.test(contentType)) {
-    error = new Error(`Invalid content-type.\n` +
-                      `Expected application/json but received ${contentType}`);
-  }
-  if (error) {
-    console.log(error.message);
-    // consume response data to free up memory
-    res.resume();
-    return;
-  }
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => rawData += chunk);
-  res.on('end', () => {
-    try {
-      let parsedData = JSON.parse(rawData);
-      console.log(parsedData);
-    } catch (e) {
-      console.log(e.message);
-    }
-  });
-}).on('error', (e) => {
-  console.log(`Got error: ${e.message}`);
+http.get('http://13.76.85.160:443/notifyusers', function(res){
+    assert(200, res.statusCode)
+    done();
 });
