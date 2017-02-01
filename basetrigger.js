78ba4b3d3bd374b3f72f9ee30bec1ce828b9462d
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-01 17:09:34 +0800

    Add files via upload

--- basetrigger.js
+++ basetrigger.js
@@ -17,7 +17,7 @@ var body = JSON.stringify({
 })
 
 var request = new http.ClientRequest({
-    hostname: "13.76.85.160",
+    hostname: "http://13.76.85.160",
     port: 443,
     path: "/notifyusers",
     method: "POST",
