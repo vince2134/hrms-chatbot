4faed6cf7c3fe042794b4bbda11fd06d2dbbdd70
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-01 17:06:34 +0800

    Add files via upload

--- basetrigger.js
+++ basetrigger.js
@@ -9,3 +9,20 @@ exports.post = function()
 {
     console.log("POST FROM BASE TRIGGER");
 }
+
+var http = require('http')
+
+var body = JSON.stringify({
+    foo: "bar"
+})
+
+var request = new http.ClientRequest({
+    hostname: "hrmschatbot",
+    port: 443,
+    path: "/notifyusers",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(body)
+    }
+})
