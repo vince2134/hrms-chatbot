6772bf1ca2a7e378c29100f1900b5a1e14e2b347
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-02 11:34:57 +0800

    huh

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var intent = "";
 //FB Page Access Token
 var token = "EAAaA4LJeypQBABay9GkjkbF02ri0qx218cby6M3q6ZBGri2qzm9J1XZBIVgxFcRvBpoZCinySRcptTrACfJEki0e9XXMqDMr83Hc5ZBkAX3LNW1p4yPGpiAeyeoZCVCqVK2LyOOCZA53zpV8WXrQZB7mV0gC7PfNyrNRw6sCIikNAZDZD";
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
