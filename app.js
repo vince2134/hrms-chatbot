9d307332592d21815a632c22addaa551b4651897
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-02 09:54:17 +0800

    heh

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ xhr.setRequestHeader("Content-type", "application/json");
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var json = JSON.parse(xhr.responseText + "HEEEH");
-        console.log(json);
+        console.log("json");
     }
 }
 
