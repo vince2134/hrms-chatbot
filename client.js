bcf70bae9aed3bb3591c9054956d9e84723c1227
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-03 14:18:24 +0800

    Add files via upload

---
+++ client.js
@@ -0,0 +1,12 @@
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
+ 
+
+var args = {
+    parameters: {id : 32}
+}
+
+client.get("http://192.168.30.210:8080/opentides/request-password-reset/get",  function (data, response) {
+    console.log(data);
+});
