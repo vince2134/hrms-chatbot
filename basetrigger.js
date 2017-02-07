022af1324d4715f9025441f736c69d9ee60f5e8f
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-07 13:53:27 +0800

    Trial commit

---
+++ basetrigger.js
@@ -0,0 +1,26 @@
+exports.answer = 42;
+
+exports.start = function(funcname)
+{
+    console.log("Start of " + funcname);
+}
+
+exports.end = function(funcname)
+{
+    console.log("End of " + funcname);
+}
+
+
+
+
+
+/*var http = require('http')
+
+var body = JSON.stringify({
+    foo: "bar"
+})
+
+http.get('http://13.76.85.160:443/notifyusers', function(res){
+    assert(200, res.statusCode)
+    done();
+});*/
