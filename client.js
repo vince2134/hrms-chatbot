5326b4e253a2f0ea5d50bd7f985fbccee51eab13
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-07 13:53:59 +0800

    trial commit 2

---
+++ client.js
@@ -0,0 +1,12 @@
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+
+var args = {
+    parameters: {id : 32}
+}
+
+client.get("http://192.168.30.210:8080/opentides/chatbot-leave/get",  function (data, response) {
+    console.log(data.toString('utf8'));
+});
