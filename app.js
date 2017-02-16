fc4fb67a4330bbf0677f3ffacc3eff8a827e02fd
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-16 15:59:16 +0800

    huh

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ function updateIntent() {
         method: 'POST',
       headers: {
                 "Authorization": "Bearer 05411b958f3840019c2e968e3ac72a63",
-                "Content-Type": "application/json; charset=utf-8"
+                "Content-Type": "X-www-form-urlencoded"
             },
             qs:
 {
