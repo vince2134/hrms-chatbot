726976141801d539f0d4d906c63ddd5605fff313
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-16 14:11:02 +0800

    huh

--- app.js
+++ app.js
@@ -240,11 +240,94 @@ function updateIntent() {
     console.log("update");
     options = {
         url: 'https://api.api.ai/v1/intents?v=20150910',
-        method: 'PUT',
-        qs: {
-            'headers': {
-                'Authorization': "Bearer 05411b958f3840019c2e968e3ac72a63"
+        method: 'POST',
+      headers: {
+                Authorization: "Bearer 05411b958f3840019c2e968e3ac72a63",
+                Content-Type: "application/json; charset=utf-8"
+            },
+            body:
+{
+   "name": "change appliance state",
+   "auto": true,
+   "contexts": [],
+   "templates": [
+      "turn @state:state the @appliance:appliance ",
+      "switch the @appliance:appliance @state:state "
+   ],
+   "userSays": [
+      {
+         "data": [
+            {
+               "text": "turn "
+            },
+            {
+               "text": "on",
+               "alias": "state",
+               "meta": "@state"
+            },
+            {
+               "text": " the "
+            },
+            {
+               "text": "kitchen lights",
+               "alias": "appliance",
+               "meta": "@appliance"
             }
+         ],
+         "isTemplate": false,
+         "count": 0
+      },
+      {
+         "data": [
+            {
+               "text": "switch the "
+            },
+            {
+               "text": "heating",
+               "alias": "appliance",
+               "meta": "@appliance"
+            },
+            {
+               "text": " "
+            },
+            {
+               "text": "off",
+               "alias": "state",
+               "meta": "@state"
+            }
+         ],
+         "isTemplate": false,
+         "count": 0
+      }
+   ],
+   "responses": [
+      {
+         "resetContexts": false,
+         "action": "set-appliance",
+         "affectedContexts": [
+            {
+               "name": "house",
+               "lifespan": 10
+            }
+         ],
+         "parameters": [
+            {
+               "dataType": "@appliance",
+               "name": "appliance",
+               "value": "\$appliance"
+            },
+            {
+               "dataType": "@state",
+               "name": "state",
+               "value": "\$state"
+            }
+         ],
+         "speech": "Turning the \$appliance \$state\!"
+      }
+   ],
+   "priority": 500000
+}
+
         }
     }
     request(options, function(error, response, body) {
