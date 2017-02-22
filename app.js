212e910b73f25d3c08992607e52a8fbdae55f221
NicolleMagpale <nicolle_magpale@dlsu.edu.ph>
2017-02-22 10:12:08 +0800

    asda

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ function callSendAPI(messageData) {
             var messageId = body.message_id;
             console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
             console.log("Successfully sent generic message: %s", messageData.message.text);
-            console.log(messageData.message);
+            console.log(messageData);
         } else {
             console.error("Unable to send message. " + messageData.message.text);
             console.error(messageData.message);
