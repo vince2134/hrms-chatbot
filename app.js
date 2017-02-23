e922d12b2e5a69d4d734083bc2626e59d928b4f2
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-23 09:34:07 +0800

    test

--- app.js
+++ app.js
@@ -435,12 +435,12 @@ function formatLeave(response, fbId)
                        'reason': response.result.parameters.reason
                     }
             };
-           console.log("leave format = " + JSON.parse(leaveFormat));
+           console.log("leave format = " + JSON.stringify(leaveFormat));
        }
        else {
        }
    });
-        
+
 }
 
 
