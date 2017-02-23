533d1ebc72456270d1a95846dea1f49f73c06395
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-02-23 14:16:56 +0800

    update

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ function registerUser(email, senderID) {
     };
 
     var options = {
-        url: 'http://192.168.30.210:8080/opentides/register',
+        url: 'http://192.168.30.210:8080/opentides/chatbot-user/register',
         method: 'GET',
         qs: {
             'emailAddress': email
